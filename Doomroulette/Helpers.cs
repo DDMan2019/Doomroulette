f9d338c6a1fb2f118d7a51553397656e101b2127
DD <danieljansson9@gmail.com>
2019-10-29 21:35:53 +0100

    Removed some unused code

--- Doomroulette/Helpers.cs
+++ Doomroulette/Helpers.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Text;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Doomroulette
 {
     class Helpers
     {
-        public static DateTime today = DateTime.Today;
-
+        
         public static DateTime FromUnixTime(long unixTime)
         {
             return epoch.AddSeconds(unixTime);
@@ -32,48 +24,6 @@ namespace Doomroulette
                 
             return true;
         }
-        /*
-        private static readonly PrivateFontCollection privateFontCollection = new PrivateFontCollection();
-        [DllImport("gdi32.dll")]
-        private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pvd, [In] ref uint pcFonts);
-
-        public static FontFamily LoadFont(byte[] fontResource)
-        {
-            int dataLength = fontResource.Length;
-            IntPtr fontPtr = Marshal.AllocCoTaskMem(dataLength);
-            Marshal.Copy(fontResource, 0, fontPtr, dataLength);
-
-            uint cFonts = 0;
-            AddFontMemResourceEx(fontPtr, (uint)fontResource.Length, IntPtr.Zero, ref cFonts);
-            privateFontCollection.AddMemoryFont(fontPtr, dataLength);
-
-            return privateFontCollection.Families.Last();
-        }
-        
-        /*
-        public static void log(string str)
-        {
-            try
-            {
-                Console.WriteLine(today + ": " + str);
-                if (!Directory.Exists("logs"))
-                {
-                    Directory.CreateDirectory("logs");
-                }
-
-                StringBuilder sb = new StringBuilder();
-                sb.AppendLine(today + ": " + str);
-                string filename = "logs/log" + today.ToString("yyyyMMdd") + ".txt";
-
-                File.AppendAllText(filename, sb.ToString());
-                sb.Clear();
-            }
-            catch (Exception e)
-            {
-                log(str);
-            }
-
-        }
-        */
+       
     }
 }
